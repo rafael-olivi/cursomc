b5e640ffb92fbc89779f3bb2d264fba01204dcb2
Timba <rbolivi@hotmail.com>
2020-11-05 23:53:28 -0300

    Created Categoria Domain and Resource

---
+++ src/main/java/com/timba/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,27 @@
+package com.timba.cursomc.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.timba.cursomc.domain.Categoria;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+
+	@RequestMapping(method=RequestMethod.GET)
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		return list;
+	}
+}
